c2f2fa169044edc0cc7dd4f285fe1472ea39c62d
Le Binh An <lvba2102@gmail.com>
2024-06-20 20:58:55 +0700

    Tokenize user's auth info with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,32 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   healthCheck() {
     const status: string = 'good';
     return `Module Auth Service is ${status}'`;
   }
 
-  signIn(userName: string, password: string): Promise<any> {
-    const user = this.userService.findOne(userName, password);
+  async signIn(userName: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(userName, password);
 
     if (user === undefined) {
       throw new UnauthorizedException();
     }
 
-    return user;
+    const payload = {
+      userName: user.name,
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
